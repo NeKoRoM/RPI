679e4b9e0ab9512411ed01d05defb9b3b836f49d
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2024-12-14 14:07:00 +0200

    2

--- dobot/dobot-python/project/main.py
+++ dobot/dobot-python/project/main.py
@@ -44,7 +44,7 @@ bot.set_device_name(device_name)
 
 print(bot.get_pose())
 # One axis at a time
-bot.set_point_to_point_command(3, 10, 10, 10, 10)
+bot.set_point_to_point_command(3, 100, 10, 10, 10)
 sleep(1)
 
 # One axis at a time
